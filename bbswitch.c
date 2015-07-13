4429d849e7997ae2c6ba91b2f2389f0f65161ae8
gnu3ra <aballmer42@gmail.com>
2015-07-12 22:22:51 -0500

    Maybe return 0

--- bbswitch.c
+++ bbswitch.c
@@ -344,7 +344,7 @@ static int bbswitch_proc_open(struct inode *inode, struct file *file) {
 
 static int bbswitch_pm_handler(struct notifier_block *nbp,
     unsigned long event_type, void *p) {
-  return -1;
+  return 0;
     switch (event_type) {
     case PM_HIBERNATION_PREPARE:
     case PM_SUSPEND_PREPARE:
